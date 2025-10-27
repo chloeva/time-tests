208701cac51489129c8380aa86df1b2fd12a6654
chloeva <chloe.allen.22@ucl.ac.uk>
2025-10-27 11:07:56 +0000

    add new function for testing no overlap

--- test_times.py
+++ test_times.py
@@ -3,5 +3,11 @@ from times import compute_overlap_time, time_range
 def test_generic_case():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    expected = short
     assert compute_overlap_time(large, short) == expected
+
+def test_no_overlap():
+    range1 = [("2025-10-27 10:00:00", "2025-10-27 11:00:00")]
+    range2 = [("2025-10-27 12:00:00", "2025-10-27 13:00:00")]
+    expected =[]
+    assert compute_overlap_time(range1, range2) == expected
