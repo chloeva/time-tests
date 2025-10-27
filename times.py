4fc55fc225863f736569ec00266c60bbef00def0
chloeva <chloe.allen.22@ucl.ac.uk>
2025-10-27 11:24:49 +0000

    adding low<high comparison after each iteration i.e. bug fix

--- times.py
+++ times.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
@@ -10,14 +9,14 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
                  for i in range(number_of_intervals)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
-
 def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
             low = max(start1, start2)
             high = min(end1, end2)
-            overlap_time.append((low, high))
+            if low < high:  # Only add if there’s a real overlap
+                overlap_time.append((low, high))
     return overlap_time
 
 if __name__ == "__main__":
